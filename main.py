7ae2c7e0ed694df154a52679e64165bab6785466
VincentPater <vincentpater@gmail.com>
2024-12-03 22:14:45 +0100

    Moved original proof of concept into a seperate module

---
+++ main.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 22:11:03 2024
+
+@author: Vincent
+"""
+
+# Import Modules
+import serial_communication as sc
+
+
+
+
+#  Main
+def main():
+    sc.init_serial_communication()
+    
+    sc.loopcycle_serial_communication()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ serial_communication.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 21:59:03 2024
+
+@author: Vincent
+"""
+import serial
+import string
+
+
+def init_serial_communication(dir_of_serial_port = '/dev/ttyUSB0'):    
+    baudrate = 115200
+    bytesize = 8
+    parity   = 'N'
+    stopbits = 1
+    stoptime = 1 # seconds
+    serialObj   = serial.Serial(dir_of_serial_port, baudrate, bytesize, parity, stopbits, timeout=stoptime)
+    
+    return serialObj
+
+
+
+
+
+
+def loopcycle_serial_communication(serialObj):
+    output = " "
+    print("---")
+    while output != "":
+        output = serialObj.readline().decode('ascii')
+        print(output)
+
+    
+    pass
